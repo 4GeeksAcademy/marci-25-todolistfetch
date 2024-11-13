869cd0509e7de4ada0f6e98698a993e0a58468f6
Marcel Rivero <126285355+Marci25@users.noreply.github.com>
2024-11-13 22:29:15 +0000

    character details

--- src/front/js/pages/Character.jsx
+++ src/front/js/pages/Character.jsx
@@ -22,6 +22,16 @@ export const Character = () => {
                     <img src={`https://starwars-visualguide.com/assets/img/characters/${characterInfo.uid}.jpg`} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{characterInfo.properties.name}</h5>
+                        <p className="card-texxt">{characterInfo.properties.birth_year}</p>
+                        <p className="card-texxt">{characterInfo.properties.gender}</p>
+                        <p className="card-texxt">{characterInfo.properties.hair_color}</p>
+                        <p className="card-texxt">{characterInfo.properties.height}</p>
+                        <p className="card-texxt">{characterInfo.properties.skin_color}</p>
+                        <p className="card-texxt">{characterInfo.properties.homeworld}</p>
+                        <p className="card-texxt">{characterInfo.properties.skin_films}</p>
+                        <p className="card-texxt">{characterInfo.properties.skin_species}</p>
+                        <p className="card-texxt">{characterInfo.properties.skin_vehicles}</p>   
+                        <p className="card-texxt">{characterInfo.properties.created}</p>   
                     </div>
                 </div>
                         <button onClick={handleOnClick()} className="btn btn-primary"> somewhere</button>
